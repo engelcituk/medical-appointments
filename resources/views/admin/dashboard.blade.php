8b796f3e05c110c0099657763d46f66683797ab7
eCituk <citukcaamal@gmail.com>
2025-11-01 03:35:40 -0500

    breadcrumbs

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,3 +1,19 @@
-<x-admin-layout>
-hola desde el admin
+{{-- texto plano, los props no llevan :, si llevan si es contenido php --}}
+<x-admin-layout
+    title="Dashboard"
+    :breadcrumbs="[
+        [
+            'name' => 'Dashboard',
+            'href' => route('admin.dashboard')
+        ],
+        [
+            'name' => 'Prueba',
+        ]
+    ]">
+    <x-slot name="action">
+        <x-button>
+            Prueba
+        </x-button>
+    </x-slot>
+
 </x-admin-layout>
