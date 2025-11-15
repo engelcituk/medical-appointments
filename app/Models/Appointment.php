f71dc62d685e468b610c39d86f6acb472f643e97
eCituk <citukcaamal@gmail.com>
2025-11-15 14:54:21 -0500

    migraciones para citas

---
+++ app/Models/Appointment.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Enums\AppointmentStatus;
+
+class Appointment extends Model
+{
+    protected $fillable = [
+        'patient_id',
+        'doctor_id',
+        'date',
+        'start_time',
+        'end_time',
+        'duration',
+        'reason',
+        'status',
+    ];
+
+    protected $casts = [
+        'date' => 'date',
+        'start_time' => 'time',
+        'end_time' => 'time',
+        'status' => AppointmentStatus::class
+    ];
+
+    public function patient()
+    {
+        return $this->belongsTo(Patient::class);
+    }
+
+    public function doctor()
+    {
+        return $this->belongsTo(Doctor::class);
+    }
+}
